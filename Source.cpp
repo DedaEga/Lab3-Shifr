9eecc7bb25563bdd3defb7229c03d228d943de88
DedaEga <Egor.Boiarshinov@urfu.me>
2022-11-28 01:03:44 +0500

    Добавьте файлы проекта.

---
+++ Source.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <windows.h>
+#include <string>
+using namespace std;
+
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	int shag;
+	string text;
+	cout << "SHifr Cezarya\nVvedite shag sdviga: ";
+	cin >> shag;
+}
