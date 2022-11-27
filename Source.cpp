89a418ece6169bf37c21b17cd5ed7dd06ca0cd65
DedaEga <Egor.Boiarshinov@urfu.me>
2022-11-28 01:45:43 +0500

    Выбор функции

--- Source.cpp
+++ Source.cpp
@@ -8,10 +8,35 @@ int main()
 {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
-	int shag;
+	int shag, choise;
 	string text;
-	cout << "SHifr Cezarya\nVvedite shag sdviga: ";
-	cin >> shag;
-	cout << "Vvedite tekst dlya zashifrovki: ";
-	cin >> text;
+	cout << "SHifr Cezarya\n";
+	while (true) {
+		cout << "Vvedite:\n1 - zashifrovat' tekst\n2 - rasshifrovat' tekst\n3 - vyjti\nVypolnit': ";
+		cin >> choise;
+		if (choise == 1) {
+			cout << "Vvedite shag: ";
+			cin >> shag;
+			cout << "Vvedite tekst dlya zashifrovki: ";
+			cin >> text;
+			for (int i = 0; i < text.length(); i++) {
+
+			}
+		}
+		else if (choise == 2) {
+			cout << "Vvedite shag: ";
+			cin >> shag;
+			cout << "Vvedite tekst dlya rasshifrovki: ";
+			cin >> text;
+			for (int i = 0; i < text.length(); i++) {
+
+			}
+		}
+		else if (choise == 3) {
+			break;
+		}
+		else {
+			cout << "Vvedeno nevernoe chislo, poprobujte eshchyo raz";
+		}
+	}	
 }
