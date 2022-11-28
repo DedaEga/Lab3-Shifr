6434e26caab6ae6dd7ae749f4a484ed97679980e
DedaEga <Egor.Boiarshinov@urfu.me>
2022-11-28 20:11:12 +0500

    Доработка шифровки

--- Source.cpp
+++ Source.cpp
@@ -6,50 +6,110 @@ int main()
 {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
-	int shag, choise;
-	string text;
+	int shag;
+	string text, choise;
 	cout << "SHifr Cezarya\n";
 	while (true) {
 		cout << "Vvedite:\n1 - zashifrovat' tekst\n2 - rasshifrovat' tekst\n3 - vyjti\nVypolnit': ";
 		cin >> choise;
-		if (choise == 1) {
+		if (choise == "1") {
 			cout << "Vvedite shag: ";
 			cin >> shag;
 			cout << "Vvedite tekst dlya zashifrovki: ";
 			cin.get();
 			getline(cin, text);
 			for (int i = 0; i < text.length(); i++) {
-				if ((text[i] > 64) and (text[i] < 123)) {			//изменение кода английских символов
+				if ((text[i] > 64) and (text[i] < 91)) {			//изменение кода английских символов
 					text[i] += shag % 26;
+					if ((text[i] < 65) or (text[i] > 90)) {
+						text[i] -= 26;
+						if ((text[i] < 65) or (text[i] > 90)) {
+							text[i] += 52;
+						}
+					}
 				}
-				else if ((text[i] > -65) and (text[i] < 1)) {		//изменение кода русских символов
+				else if ((text[i] > 96) and (text[i] < 123)) {
+					text[i] += shag % 26;
+					if ((text[i] < 97) or (text[i] > 122)) {
+						text[i] -= 26;
+						if ((text[i] < 97) or (text[i] > 122)) {
+							text[i] += 52;
+						}
+					}
+				}
+				else if ((text[i] > -65) and (text[i] < -32)) {		//изменение кода русских символов
 					text[i] += shag % 32;
+					if ((text[i] < -64) or (text[i] > -33)) {
+						text[i] -= 32;
+						if ((text[i] < -64) or (text[i] > -33)) {
+							text[i] += 64;
+						}
+					}
+				}
+				else if ((text[i] > -33) and (text[i] < 0)) {		//изменение кода русских символов
+					text[i] += shag % 32;
+					if ((text[i] < -32) or (text[i] > -1)) {
+						text[i] -= 32;
+						if ((text[i] < -32) or (text[i] > -1)) {
+							text[i] += 64;
+						}
+					}
 				}
 			}
 			cout << "Zashifrovannyj tekst: " << text << endl;
 		}
-		else if (choise == 2) {
+		else if (choise == "2") {
 			cout << "Vvedite shag: ";
 			cin >> shag;
 			cout << "Vvedite tekst dlya rasshifrovki: ";
 			cin.get();
 			getline(cin, text);
 			for (int i = 0; i < text.length(); i++) {
-				if ((text[i] > 64) and (text[i] < 123)) {			
-					text[i] -= shag % 26;							//теперь вычитаем шаг
+				if ((text[i] > 64) and (text[i] < 91)) {			//изменение кода английских символов
+					text[i] -= shag % 26;
+					if ((text[i] < 65) or (text[i] > 90)) {
+						text[i] -= 26;
+						if ((text[i] < 65) or (text[i] > 90)) {
+							text[i] += 52;
+						}
+					}
 				}
-				else if ((text[i] > -65) and (text[i] < 1)) {		
+				else if ((text[i] > 96) and (text[i] < 123)) {
+					text[i] -= shag % 26;
+					if ((text[i] < 97) or (text[i] > 122)) {
+						text[i] -= 26;
+						if ((text[i] < 97) or (text[i] > 122)) {
+							text[i] += 52;
+						}
+					}
+				}
+				else if ((text[i] > -65) and (text[i] < -32)) {		//изменение кода русских символов
 					text[i] -= shag % 32;
+					if ((text[i] < -64) or (text[i] > -33)) {
+						text[i] -= 32;
+						if ((text[i] < -64) or (text[i] > -33)) {
+							text[i] += 64;
+						}
+					}
+				}
+				else if ((text[i] > -33) and (text[i] < 0)) {		//изменение кода русских символов
+					text[i] -= shag % 32;
+					if ((text[i] < -32) or (text[i] > -1)) {
+						text[i] -= 32;
+						if ((text[i] < -32) or (text[i] > -1)) {
+							text[i] += 64;
+						}
+					}
 				}
 			}
-			cout << "Zashifrovannyj tekst: " << text << endl;
+			cout << "Rasshifrovannyj tekst: " << text << endl;
 		}
-		else if (choise == 3) {
+		else if (choise == "3") {
 			break;
 		}
 		else {
 			cout << "Vvedeno nevernoe chislo, poprobujte eshchyo raz\n";
 		}
-		choise = 0;
+		choise = "";
 	}	
 }
